f9d534311b07eb9954147704deffe1979aebaade
josue <josue.quinteros@airlinemro.parts>
2025-09-07 14:35:42 -0600

    Stop skipping unit tests

--- src/test/java/com/applaudo/project/controller/IceCreamControllerTest.java
+++ src/test/java/com/applaudo/project/controller/IceCreamControllerTest.java
@@ -204,7 +204,6 @@ class IceCreamControllerTest {
         }
 
         @Test
-        @Disabled 
         void createIceCreamTest() throws Exception {
             String name = "Test";
             BigDecimal cost = new BigDecimal("15.00");
